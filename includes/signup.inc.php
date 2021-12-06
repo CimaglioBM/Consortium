7633b297ed66115ce311c3fd00482f41ce3b4b52
CimaglioBM <94077497+CimaglioBM@users.noreply.github.com>
2021-12-06 14:18:37 -0600

    Update signup.inc.php
    This will take the Name, Email, and Password from the Signup Page and insert them into an SQL database. If you want to change the names of the SQL bases be sure to match the  DB_Name at the top, as well as the actual table name in the Insert statement.
    
    The only thing I cannot get is when you submit it brings you to a new page, and I am not sure why

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,7 +1,25 @@
 <?php
-echo "here";
-if (isset($_POST["submit"])) {
-    echo "It works";
-} else {
-    header("location: ../signup.php");
+// Defined as constants so that they can't be changed
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PASSWORD', '');
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_NAME', 'consortium_people');
+// $dbc will contain a resource link to the database
+// @ keeps the error from showing in the browser
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+OR die('Could not connect to MySQL: ' .
+mysqli_connect_error());
+echo "Connection Successful";
+
+
+$sql = "INSERT INTO people (Name, Email, Password) 
+                    VALUES ('" .$_POST["username"]."', '".$_POST["email"]."', '" .$_POST["password"]."')";
+
+if(mysqli_query($dbc, $sql)){
+    echo "<br> Records inserted successfully.";
+} else{
+    echo "<br> ERROR: Could not able to execute $sql. ";
 }
+
+?>
+
